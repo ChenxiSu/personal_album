180c9732c8a8fabf790106fa5414cc193c707479
ChenxiSu <cs2238@cornell.edu>
2016-03-10 13:24:59 -0500

    typo in featureGeneration folder

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>My personal album</title>
+		<link rel="stylesheet" type="text/css" href="featureGeneration/areaGeneration.css">
+	</head>
+	<body>
+		<div id="container">
+			<div id="nav">
+			</div>
+
+			<div id="header">
+			</div>
+
+			<div id="content">
+			</div>
+
+			<div id="footer">
+			</div>
+		</div>
+	</body>
+</html>
